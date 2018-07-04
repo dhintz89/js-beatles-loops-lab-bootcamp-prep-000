04023f3c7010056a572972e631b2c77df45d3ad4
Dan Hintz <dhintz@live.com>
2018-07-04 00:32:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const array = [];
+  for(let i=0; i<=musicians.len();i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  
+  while(
+}
