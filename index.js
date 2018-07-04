77e8d6b7888bb09862afc4e64f7097e24e27c97d
Dan Hintz <dhintz@live.com>
2018-07-04 00:38:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,10 @@ function johnLennonFacts(facts) {
   }
 }
 
-iLoveThe
+function iLoveTheBeatles(number) {
+  var array=[];
+  do {
+    array.push("I love the Beatles!");
+  } while(number<15);
+  return array;
+}
